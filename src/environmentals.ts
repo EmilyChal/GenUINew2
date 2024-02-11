070d62c40b1e63e4c566aedffdc1aa28bef329d7
EmilyChal <x6n73@students.keele.ac.uk>
2024-02-11 18:22:04 +0200

    PROD Settings

--- src/environmentals.ts
+++ src/environmentals.ts
@@ -1,2 +1,5 @@
 export const GEN_API_LOCAL = 'http://localhost:5142'
 export const AUTH_API_LOCAL = 'http://localhost:5262'
+
+export const GEN_API_PROD = 'http://18.169.194.246:5000'
+export const AUTH_API_PROD = 'http://35.178.202.112:5100'
