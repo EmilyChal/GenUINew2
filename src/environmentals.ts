423663ae8ef18c0a58e2a2765e1924ded88e115f
EmilyChal <x6n73@students.keele.ac.uk>
2024-02-11 18:41:14 +0200

    New Ip

--- src/environmentals.ts
+++ src/environmentals.ts
@@ -2,4 +2,4 @@ export const GEN_API_LOCAL = 'http://localhost:5142'
 export const AUTH_API_LOCAL = 'http://localhost:5262'
 
 export const GEN_API_PROD = 'http://18.169.194.246:5000'
-export const AUTH_API_PROD = 'http://35.178.202.112:5100'
+export const AUTH_API_PROD = 'http://18.130.83.111:5100'
