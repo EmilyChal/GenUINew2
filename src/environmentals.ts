bc0e3592f8b74f308c9ecdb679680e04c984d622
EmilyChal <x6n73@students.keele.ac.uk>
2024-03-03 14:42:07 +0200

    fix

--- src/environmentals.ts
+++ src/environmentals.ts
@@ -1,5 +1,5 @@
 export const GEN_API_LOCAL = 'http://localhost:5142'
 export const AUTH_API_LOCAL = 'http://localhost:5262'
 
-export const GEN_API_PROD = 'http://18.169.189.219:5000'
+export const GEN_API_PROD = 'http://3.11.70.114:5000'
 export const AUTH_API_PROD = 'http://3.8.232.112:5100'
